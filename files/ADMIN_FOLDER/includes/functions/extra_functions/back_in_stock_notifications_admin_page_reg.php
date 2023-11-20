bf80587e4dfa8d860e2d808247ae38400d6678a9
Steve <4391026+torvista@users.noreply.github.com>
2023-11-20 13:07:41 +0100

    fix: cron error, check zen_page_key_exists

--- files/ADMIN_FOLDER/includes/functions/extra_functions/back_in_stock_notifications_admin_page_reg.php
+++ files/ADMIN_FOLDER/includes/functions/extra_functions/back_in_stock_notifications_admin_page_reg.php
@@ -19,39 +19,42 @@ declare(strict_types=1);
  * @copyright   Portions Copyright 2003 osCommerce
  * @link        https://www.dev.ceon.net
  * @license     https://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version     $Id: back_in_stock_notifications_admin_page_reg.php 2023-11-11 torvista
+ * @version     $Id: back_in_stock_notifications_admin_page_reg.php 2023-11-20 torvista
  */
 
 if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
+// if a cron script calls application_top, this function may not be available at this point
+if (function_exists('zen_page_key_exists')) {
+    if (!zen_page_key_exists('ceon_bisn')) {
+        // Register the Ceon Back In Stock Notifications Admin Page (not configuration page) with the Zen Cart admin
 
-if (!zen_page_key_exists('ceon_bisn')) {
-    // Register the Ceon Back In Stock Notifications Admin Page (not configuration page) with the Zen Cart admin
-
-    // Sanity check in case user hasn't uploaded a necessary file
-    $error_messages = [];
-    if (!defined('FILENAME_CEON_BACK_IN_STOCK_NOTIFICATIONS')) {
-        $error_messages[] = 'The Back In Stock Notifications filename define "FILENAME_CEON_BACK_IN_STOCK_NOTIFICATIONS" is missing.' .
-            ' Please check that the file ' . DIR_WS_INCLUDES . 'extra_datafiles/' . 'back_in_stock_notifications_filenames.php has been uploaded.';
-    }
-
-    if (sizeof($error_messages) > 0) {
-        // Let the user know that there are problem(s) with the installation
-        foreach ($error_messages as $error_message) {
-            print '<p style="background: #fcc; border: 1px solid #f00; margin: 1em;' . ' padding: 0.4em;">Error: ' . $error_message . "</p>\n";
+        // Sanity check in case user hasn't uploaded a necessary file
+        $error_messages = [];
+        if (!defined('FILENAME_CEON_BACK_IN_STOCK_NOTIFICATIONS')) {
+            $error_messages[] = 'The Back In Stock Notifications filename define "FILENAME_CEON_BACK_IN_STOCK_NOTIFICATIONS" is missing.' .
+                ' Please check that the file ' . DIR_WS_INCLUDES . 'extra_datafiles/' . 'back_in_stock_notifications_filenames.php has been uploaded.';
         }
 
-    } else {
-        // The necessary file is in place, so proceed to register the Admin->Catalog->BISN admin page/create the menu item
-        zen_register_admin_page(
-            'ceon_bisn',
-            'BOX_CEON_BACK_IN_STOCK_NOTIFICATIONS',
-            'FILENAME_CEON_BACK_IN_STOCK_NOTIFICATIONS',
-            '',
-            'catalog',
-            'Y'
-        );
-        $messageStack->add('BISN Installer: menu item added "Catalog->Ceon Back In Stock Notifications"', 'success');//note language definitions are not available at this stage, so need to use hard-coded texts
+        if (sizeof($error_messages) > 0) {
+            // Let the user know that there are problem(s) with the installation
+            foreach ($error_messages as $error_message) {
+                print '<p style="background: #fcc; border: 1px solid #f00; margin: 1em;' . ' padding: 0.4em;">Error: ' . $error_message . "</p>\n";
+            }
+        } else {
+            // The necessary file is in place, so proceed to register the Admin->Catalog->BISN admin page/create the menu item
+            zen_register_admin_page(
+                'ceon_bisn',
+                'BOX_CEON_BACK_IN_STOCK_NOTIFICATIONS',
+                'FILENAME_CEON_BACK_IN_STOCK_NOTIFICATIONS',
+                '',
+                'catalog',
+                'Y'
+            );
+            // note language definitions are not available at this stage, so need to use hard-coded texts
+            $messageStack->add('BISN Installer: menu item added "Catalog->Ceon Back In Stock Notifications"', 'success');
+        }
     }
 }
+
